2f2749b1066202522081aa5dfae9f8b3484b4e01
tintoy <tintoy@tintoy.io>
2017-01-08 17:23:57 +1100

    Add some initial tests.

---
+++ test/AWTY.Core.Tests/Int32ProgressSinkTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xunit;
+
+namespace AWTY.Core.Tests
+{
+    /// <summary>
+    ///     Tests for <see cref="Int32ProgressSink"/>.
+    /// </summary>
+    public class Int32ProgressSinkTests
+    {
+        [Fact]
+        public void Ctor_Total_100() 
+        {
+            IProgressSink<int> progressSink = new Int32ProgressSink(total: 100);
+            Assert.Equal(100, progressSink.Total);
+            Assert.Equal(0, progressSink.Current);
+            Assert.Equal(0, progressSink.PercentComplete);
+        }
+
+        [Fact]
+        public void Ctor_Total_MaxValue() 
+        {
+            IProgressSink<int> progressSink = new Int32ProgressSink(total: Int32.MaxValue);
+            Assert.Equal(0, progressSink.Current);
+            Assert.Equal(Int32.MaxValue, progressSink.Total);
+            Assert.Equal(0, progressSink.PercentComplete);
+        }
+
+        [Fact]
+        public void Ctor_Total_0() 
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => new Int32ProgressSink(total: 0)
+            );
+        }
+
+        [Fact]
+        public void Ctor_Total_Negative1() 
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => new Int32ProgressSink(total: -1)
+            );
+        }
+    }
+}
---
+++ test/AWTY.Core.Tests/Int64ProgressSinkTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Xunit;
+
+namespace AWTY.Core.Tests
+{
+    /// <summary>
+    ///     Tests for <see cref="Int64ProgressSink"/>.
+    /// </summary>
+    public class Int64ProgressSinkTests
+    {
+        [Fact]
+        public void Ctor_Total_100() 
+        {
+            IProgressSink<long> progressSink = new Int64ProgressSink(total: 100L);
+            Assert.Equal(100L, progressSink.Total);
+            Assert.Equal(0L, progressSink.Current);
+            Assert.Equal(0L, progressSink.PercentComplete);
+        }
+
+        [Fact]
+        public void Ctor_Total_MaxValue() 
+        {
+            IProgressSink<long> progressSink = new Int64ProgressSink(total: Int64.MaxValue);
+            Assert.Equal(Int64.MaxValue, progressSink.Total);
+            Assert.Equal(0L, progressSink.Current);
+            Assert.Equal(0L, progressSink.PercentComplete);
+        }
+
+        [Fact]
+        public void Ctor_Total_0() 
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => new Int64ProgressSink(total: 0)
+            );
+        }
+
+        [Fact]
+        public void Ctor_Total_Negative1() 
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => new Int64ProgressSink(total: -1)
+            );
+        }
+    }
+}
