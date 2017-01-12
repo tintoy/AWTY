e5d9d3c19e3f39e2ddc37449e0a54809aa79a5fb
tintoy <tintoy@tintoy.io>
2017-01-12 13:07:08 +1100

    Add link to MemoryStream.CopyToAsync source link.

--- test/AWTY.Http.IntegrationTests/DumbMemoryStream.cs
+++ test/AWTY.Http.IntegrationTests/DumbMemoryStream.cs
@@ -6,9 +6,10 @@ namespace AWTY.Http.IntegrationTests
     ///     A non-optimised version of <see cref="MemoryStream"/> for use in tests.
     /// </summary>
     /// <remarks>
-    ///     <see cref="MemoryStream"/> performs a bunch of optimisations unless subclassed.
+    ///     <see cref="MemoryStream"/>'s CopyToAsync performs a bunch of optimisations unless subclassed.
+    ///     https://github.com/Microsoft/referencesource/blob/master/mscorlib/system/io/memorystream.cs#L450
     /// 
-    ///     Unfortunately, these optimisations break the tests.
+    ///     Unfortunately, these optimisations break some of our tests.
     ///     Although you obviously wouldn't use a MemoryStream in real life, it's the simplest options for these tests.
     /// </remarks>
     public sealed class DumbMemoryStream
